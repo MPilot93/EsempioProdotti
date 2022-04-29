9847c7dd64abb7d32db22901f22f2b2c10a413a0
MPilot93 <103995096+MPilot93@users.noreply.github.com>
2022-04-29 17:09:32 +0200

    Add files via upload

---
+++ FileHelp.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+namespace FileHelper
+{
+    public class Helper
+    {
+
+        private readonly string Path;
+        public Helper(string path)
+        {
+            Path = path;
+        }
+        public void AddContact(listaprodotti list)
+        {
+            var result = $"{list.id};{list.nome};{list.magazzino}";
+
+            using (var stream = new StreamWriter(Path, true))
+            {
+                stream.WriteLine(result);
+            }
+        }
+
+        public List<Contact> ReadAll()
+        {
+            var nlista = new List<list>();
+
+            using (var stream = new StreamReader(Path))
+            {
+                var header = "nome;cognome;telefono;indirizzo";
+
+                var firstLine = stream.ReadLine();
+                if (!firstLine.Equals(header))
+                    throw new Exception("File non conforme!");
+
+
+                while (!stream.EndOfStream)
+                {
+                    var row = stream.ReadLine();
+                    var splitted = row.Split(";");
+
+                    var list = new list
+                    {
+                        id = splitted[0],
+                        nome = splitted[1],
+                        magazzino = splitted[2],
+                      
+                    };
+
+                    list.Add(list);
+                }
+
+                return list;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using FileHelper;
+
+var fileHelper = new Helper(@"C:\Users\student\Desktop\ListaProdotti.csv");
+
+var list = fileHelper.ReadAll();
+
+Console.WriteLine("*********************");
+
+foreach (var list in list)
+{
+    Console.WriteLine($"idProdotto: {list.id} - nome: {list.nome} - magazzino: {list.magazzino}");
+}
+
+Console.WriteLine("*********************");
+
+Console.WriteLine("Inserisci id prodotto");
+var id = Console.ReadLine();
+Console.WriteLine("Inserisci il nome prodotto");
+var nome = Console.ReadLine();
+Console.WriteLine("Inserisci quantità magazzino");
+var magazzino = Console.ReadLine();
+
+
+var listsNew = new list();
+listsNew.id = id;
+listsNew.nome = nome;
+listsNew.magazzino = magazzino;
+
+
+fileHelper.AddContact(contactnew);
+
+list = fileHelper.ReadAll();
+
+
+Console.WriteLine("*********************");
+
+foreach (var list in list)
+{
+    Console.WriteLine($"id: {list.id} - nome: {list.nome} - quantità presente: {list.magazzino}");
+}
+
+Console.WriteLine("*********************");
+
+
+Console.ReadLine();
